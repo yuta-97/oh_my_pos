45f2d72be579156b364aae15989b1da544ca3403
cozy-ho <kj2693119@gmail.com>
2020-09-27 14:43:35 +0900

    [Mod] apply bootstrap & make base templete

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
 import router from './router'
 import store from './store'
 
+//Register BootstrapVue in entry point
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 new Vue({
-  vuetify,
   router,
   store,
   render: h => h(App)
--- frontend/src/views/ManMain.vue
+++ frontend/src/views/ManMain.vue
@@ -1,59 +1,23 @@
 <template>
-  <div class="inner-wrap" fluid fill-height>
-    <v-app id="inspire">
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-      >
-      <v-list dense>
-        <v-list-item :key="i" link @click="$router.push('/Pos')">
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title> 포스 </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item v-on:click="selmenus = 'ManStore'">
-          <v-list-item-action>
-            <v-icon>mdi-what</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title> 매장 관리 </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item v-on:click="selmenus = 'ManCate'">
-          <v-list-item-action>
-            <v-icon>mdi-list</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title> 카테고리 관리</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item  v-on:click="selmenus = 'ManGoods'">
-          <v-list-item-action>
-            <v-icon>mdi-list</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title> 상품 관리</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item v-on:click="selmenus = 'ManQR' ">
-          <v-list-item-action>
-            <v-icon>mdi-list</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title> 큐알코드 관리</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-      </v-list>
-    </v-navigation-drawer>
+  <div>
+    <b-nav id="sidebar-right" no-header right no->
+      <template>
+        <div class="p-3">
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item link @click="$router.push('/Pos')">Pos</b-nav-item>
+              <b-nav-item active v-on:click="selmenus='ManStore'">매장 관리</b-nav-item>
+              <b-nav-item v-on:click="selmenus='ManCate'">카테고리 관리</b-nav-item>
+              <b-nav-item v-on:click="selmenus='ManGoods'">상품 관리</b-nav-item>
+              <b-nav-item v-on:click="selmenus='ManQR'">QR 관리</b-nav-item>
+            </b-nav>
+          </nav>
+        </div>
+      </template>
+    </b-nav>
     <component v-bind:is="selmenus">
-      <p>Default content</p>
-    </component>
 
-    </v-app>
+    </component>
   </div>
 </template>
 
@@ -83,3 +47,9 @@ import ManCate from '../components/ManCate.vue'
     }
   }
 </script>
+<style>
+#sidebar-right{
+  width: 20%;
+  position: fixed;
+}
+</style>
