6199f2b754e5f67454acc32fa74fa1cd06a0ba32
reemrev21 <reemrev21@gmail.com>
2020-10-09 13:10:16 +0900

    매장 추가 모달화 완료

--- frontend/src/components/ManStore.vue
+++ frontend/src/components/ManStore.vue
@@ -1,54 +1,72 @@
 <template>
   <div>
-    
-    <b-form @submit="onSubmit" @reset="onReset">
-      <b-form-group id="input-group-storename" label="Your Store Name :" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="storename"
-          required
-          placeholder="What's your store name?"
-        ></b-form-input>
-      </b-form-group>
+    <div>
+      <h2> 매장 등록 페이지 </h2>
 
-      <b-form-group id="input-group-tablenum" label="Table num :" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="tablenum"
-          required
-          placeholder="How many tables in your store?"
-        ></b-form-input>
-      </b-form-group>
+      <div>
+        <ManStoreList @openModal="openModal"></ManStoreList>
+      </div>
 
-      <b-form-group id="input-group-disname" label="할인 종류 : " label-for="input-3">
-        <b-form-input
-          id="input-3"
-          v-model="discount.disname"
-          required
-          placeholder="ex) VIP 할인"
-        ></b-form-input>
-      </b-form-group>
+      <!-- 매장 추가 모달 -->
+      <MyModal @close="closeModal" v-if="modal">
+        <b-form @submit="onSubmit" @reset="onReset">
+          <b-form-group id="input-group-storename" label="Your Store Name :" label-for="input-1">
+            <b-form-input
+              id="input-1"
+              v-model="storename"
+              required
+              placeholder="What's your store name?"
+            ></b-form-input>
+          </b-form-group>
 
-      <b-form-group id="input-group-disrate" label="할인 률 : " label-for="input-4">
-        <b-form-input
-          id="input-4"
-          v-model="discount.disrate"
-          required
-          placeholder="ex) 3.3"
-        ></b-form-input>
-      </b-form-group>
+          <b-form-group id="input-group-tablenum" label="Table num :" label-for="input-2">
+            <b-form-input
+              id="input-2"
+              v-model="tablenum"
+              required
+              placeholder="How many tables in your store?"
+            ></b-form-input>
+          </b-form-group>
 
-      <b-button type="submit" variant="primary">추가</b-button>  &nbsp;&nbsp;
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
+          <b-form-group id="input-group-disname" label="할인 종류 : " label-for="input-3">
+            <b-form-input
+              id="input-3"
+              v-model="discount.disname"
+              required
+              placeholder="ex) VIP 할인"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-disrate" label="할인 률 : " label-for="input-4">
+            <b-form-input
+              id="input-4"
+              v-model="discount.disrate"
+              required
+              placeholder="ex) 3.3"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">추가</b-button>  &nbsp;&nbsp;
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+      </MyModal>
+
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import MyModal from '../components/ManStoreModal.vue';
+import ManStoreList from '../components/ManStoreList.vue'
   export default {
+    components: {
+      ManStoreList,
+      MyModal
+    },
+    
     data() {
       return {
+        modal: false,
         storename: '',
         tablenum: '',
         discount:{
@@ -57,7 +75,16 @@ import axios from 'axios';
         }
       }
     },
+
     methods: {
+      openModal() {
+        this.modal = true
+      },
+
+      closeModal() {
+        this.modal = false
+      },
+
       onSubmit(evt) {
         evt.preventDefault()
         axios({
---
+++ frontend/src/components/ManStoreList.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+    <vue-good-table
+      @on-selected-rows-change="selectionChanged"
+      :line-numbers="true"
+      :columns="columns"
+      :rows="rows"
+      :select-options="{ 
+        enabled: true,
+      }
+      "
+      :search-options="{ enabled: true }">
+      <div slot="selected-row-actions">
+        <b-button pill variant="outline-primary" v-if="rowselected.length===1">수정</b-button>
+        <b-button pill variant="outline-danger" @click="deleteCategory">삭제</b-button>
+      </div>
+      <div slot="table-actions">
+        <b-button pill variant="success" @click="openModal">매장 추가</b-button>
+      </div>
+      </vue-good-table>
+    </div>
+</template>
+
+<script>
+// import axios from 'axios';
+import 'vue-good-table/dist/vue-good-table.css'
+import { VueGoodTable } from 'vue-good-table';
+
+export default {
+    components: {
+        VueGoodTable
+    },
+
+    data() {
+        return {
+          rowselected:[],
+          columns: [
+          {
+            label: '매 장 명',
+            field: 'store_name',
+          },
+          {
+            label: '테이블 수',
+            field: 'table_num',
+            type: 'number',
+          },
+          {
+            label: '할인 종류',
+            field: 'disname',
+          },
+          {
+            label: '할 인 률',
+            field: 'dis_rate',
+            type: 'number'
+          },
+        ],
+        
+        rows:[],
+        }
+    },
+      
+    methods: {  
+        openModal() {
+            this.$emit('openModal')
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
