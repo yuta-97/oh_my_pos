054915bea0ccb8db724c7ed555d577a1d26b3696
cozy-ho <kj2693119@gmail.com>
2020-10-11 16:44:37 +0900

    [Mod] Menu.vue

--- frontend/src/views/Menu.vue
+++ frontend/src/views/Menu.vue
@@ -8,6 +8,58 @@
         <MenuList></MenuList>
     </div> 
 
+<<<<<<< HEAD
+=======
+        <b-card-body
+          id="nav-scroller"
+          ref="content"
+          style="position: relative; height: 600px; overflow-y: scroll"
+          @click="openModal"
+        >
+        <div
+        v-for="goods in goodslist"
+        v-bind:key="goods"
+        :id="goods.category_name"
+        >
+          <b-card>
+            <div>
+              <b-img :src="goods.img_url" alt="imgs" height="90px" width="120px" style="float: left;" ></b-img>
+            </div>
+            <b-card-text>
+              <b>{{goods.goods_name}}</b><br />
+              {{goods.price}}<br />
+              {{goods.desc}}<br />
+            </b-card-text>
+          </b-card>
+        </div>
+          
+          
+          
+        </b-card-body>
+      </b-card>
+    </div>
+
+    <div class="cart">
+      <b-button> 카트 </b-button>
+    </div>
+
+    <div class="selectMenu">
+      <MyModal @close="closeModal" v-if="modal">
+        <div class="menu">
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </b-card-text>
+>>>>>>> 13bfcc8e... [Mod] Menu.vue
 
 
 
@@ -26,6 +78,7 @@ export default {
 </script>
 
 <style scoped>
+<<<<<<< HEAD
     .header {
         position: relative;
         padding: 20px;
@@ -41,7 +94,41 @@ export default {
         /* overflow-y:scroll; */
         width: 100%
     }
+=======
+.storeName {
+  float: left;
+  width: 100%;
+  height: 50px;
+}
+
+.main-content {
+  clear: left;
+  float: left;
+  width: 100%;
+  position: fixed;
+  height: 1000px;
+  overflow-y: scroll;
+}
+>>>>>>> 13bfcc8e... [Mod] Menu.vue
 
 
 
+<<<<<<< HEAD
+=======
+.footer {
+  position: absolute;
+  clear: center;
+  width: 300px;
+  height: 50px;
+  left: 0;
+  bottom: 0px;
+}
+
+/* .hr{
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        border: 0;
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
+    } */
+>>>>>>> 13bfcc8e... [Mod] Menu.vue
 </style>
