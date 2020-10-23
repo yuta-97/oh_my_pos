65696ebfacb5e9e3681c062f786fb2b125df4ce3
reemrev21 <reemrev21@gmail.com>
2020-10-23 21:06:49 +0900

    현금 카드 페이지 간단 레이아웃

--- frontend/src/components/Payment/PayCash.vue
+++ frontend/src/components/Payment/PayCash.vue
@@ -1,15 +1,53 @@
 <template>
-    <div>
-        현금
+    <div class="container">
+        <div class="wrapper">
+            <div class="top">
+                <div style="width: 50%; float: left; text-align: left;">
+                    카 드 결 제
+                </div>
+                <div style="width: 50%; float: right; text-align: right;">
+                    <b-icon icon="x-circle" scale="2" variant="danger"></b-icon>
+                </div>
+                <br/>
+                <br/>
+                <hr/>
+            </div>
+
+            <div class="body">
+               
+            </div> 
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
 }
 </script>
 
 <style scoped>
+    .container {
+        display: flex;
+    }
+
+    .top {
+        flex: 1;
+        width: 100%;
+        height: 10%;
+    }
+
+    .body {
+        flex: 1;
+        width: 100%;
+        height: 90%;
+    }
 
 </style>
